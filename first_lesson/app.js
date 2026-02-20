59132b97a081a94a06f2d0528f77eb4d98a3f782
Sobirov <sobirovazizbek69@gmail.com>
2026-02-20 11:47:34 +0500

    first lesson

---
+++ first_lesson/app.js
@@ -0,0 +1,15 @@
+console.log("5" + 5);
+console.log("5" - 5);
+console.log("5" * "2");
+console.log("10" / 2);
+console.log(5 + true);
+console.log("10" - false);
+console.log("5" + true);
+console.log(5 + null);
+console.log(5 + undefined);
+
+// Second challenge
+console.log("200" - (-50));
+console.log(25 + " years old");
+console.log("false" == true);
+console.log("10" - true);
