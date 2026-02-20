0c5b03f1e6a0c4b2916e50f2db6c7d675a1bbbad
Patrick <doniyorkhudayarov@gmail.com>
2026-02-20 11:45:07 +0500

    lessonn tasks

---
+++ app.js
@@ -0,0 +1,19 @@
+// Challenge 1
+console.log("5"+5); // 55
+console.log("5"-5); // 0
+console.log("5"*"2"); // 10
+console.log("10"/2); //5
+console.log(5 + true); // 6
+console.log("10" - true); // 9
+console.log("5" + true); // 5true
+console.log(5 + null); // 5
+console.log(5 + undefined); // NaN
+
+
+
+// Challenge 2 
+
+console.log("200" - (- 50));  // 250
+console.log(25 + " years old"); // 25 years old
+console.log("false"); // false
+console.log("10" - true); // 9
