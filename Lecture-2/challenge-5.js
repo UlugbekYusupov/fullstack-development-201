66ce4b7e51a64b8554e8f130748b44ddc2717fcf
Az1zbekx <kalilinux42331@gmail.com>
2026-02-23 16:15:39 +0500

    challenge-5

--- Lecture-2/challenge-5.js
+++ Lecture-2/challenge-5.js
@@ -1,5 +1,5 @@
 console.log(!!"false" == !!"true") // true
 console.log(0 || "JavaScript") // JavaScript
-console.log(0 && 100) // true
+console.log(0 && 100) // 0
 console.log(null ?? "Fallback") // Fallback
 console.log(undefined ?? "Default Value") // Default
