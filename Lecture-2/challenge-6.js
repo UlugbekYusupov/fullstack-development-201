17ef83f5b92b494be3063a18ddd4df5460245b41
Az1zbekx <kalilinux42331@gmail.com>
2026-02-23 16:23:33 +0500

    challenge-6

---
+++ Lecture-2/challenge-6.js
@@ -0,0 +1,8 @@
+let a = parseInt("42")
+let b = parseFloat("3.14")
+let c = parseInt("hello")
+let d = parseInt(false)
+let e = parseInt(null)
+
+let sum = a + b + c + d + e
+console.log(sum) // Nan
