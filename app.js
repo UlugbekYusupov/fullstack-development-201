508b5bdc597247b40dfdb464ec0c61867357290c
ogabek <ogbkiy@gmail.com>
2026-02-23 08:58:43 +0500

    challenges+

--- app.js
+++ app.js
@@ -48,3 +48,70 @@ console.log('10' - true)
 //  1. Yaratish bosqichi(Phase creation): O'zgaruvchilar va funksiyalar xotiradan joy oladi (hoisting) this qiymati aniqlanadi.
 //  2. Bajarish bosqichi (Execution Phase): Kod satrma-satr o'qilib, o'zgaruvchilarga qiymatlar yuklanadi va funksiyalar ishlaydi.
 // u kodning bajarilish tartibini (Call Stack) boshqaradi.
+
+
+
+
+
+const temperatureCelsius = 25.0;
+
+const tempFahrenheit = (temperatureCelsius*(9/5)) + 32
+
+console.log("cel",temperatureCelsius)
+console.log("faren", tempFahrenheit)
+
+
+// tempFahrenheit = 27
+// console.log(tempFahrenheit)
+
+// const input = prompt("enter the number: ")
+
+// const cel = parseFloat(input)
+
+// const fahren = (cel * (9/5)) + 32
+
+// console.log(`${cel} is equal to ${fahren}`)
+// alert(`${cel} is equal to ${fahren}`)
+
+
+// let budget;
+// const input = prompt("enter number")
+
+// budget = Number(input)
+
+
+// const expense1 = Number(prompt("1-chi expenseni kiriting: "))
+// const expense2 = Number(prompt("2-chi expenseni kiriting: "))
+// const expense3 = Number(prompt("3-chi expenseni kiriting: "))
+
+// const balance = budget - (expense1 + expense2 + expense3)
+
+// if(balance < 0){
+//     alert("puling yetmaydi")
+// } else {
+//     alert("pul kop hali")
+// }
+
+
+
+console.log(!!"false" == !!"true")
+console.log(0 || "JavaScript")
+console.log(" " && 100)
+
+
+console.log(null ?? "Fallback")
+
+console.log(undefined ?? "Default Value")
+
+
+let a = "42"
+let b = "3.14"
+let c = "hello"
+
+let d = false
+
+let e = null
+
+let sum = a + b + d + e
+
+console.log("sum",sum)
