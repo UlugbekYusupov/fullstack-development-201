33cbecb38a2c5714e5540be898a340f8177aea5c
Az1zbekx <kalilinux42331@gmail.com>
2026-02-23 14:11:43 +0500

    Code Runner sozlamalari test qilindi

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const readline = require("node:readline/promises")
-const { stdin: input, stdout: output } = require("node:process")
-
-const rl = readline.createInterface({ input, output })
-
-async function main() {
-  const n = await rl.question("Son kiriting: ")
-  console.log(Number(n) + 1)
-  rl.close()
-}
-
-main()
+console.log("Hello world")
