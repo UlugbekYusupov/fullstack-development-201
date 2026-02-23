617d15d8103e34c88b8b562d9e0d9ce8ba6312d3
Az1zbekx <kalilinux42331@gmail.com>
2026-02-23 10:00:51 +0500

    Javascript-operator-1-5-chellenges

---
+++ 4-dars-Javascript-operator/lesson.js
@@ -0,0 +1,52 @@
+//Challenge-1
+// let n = +prompt()
+// console.log(
+//   n % 2 == 0 
+//     ? parseInt(n / 2) + " " + parseInt(n / 2)
+//     : parseInt(n / 2) + " " + (parseInt(n / 2) + 1)
+// )
+
+
+
+//Challenge-2
+// let n = +prompt()
+// let ans = ""
+// while (n > 0) {
+//     ans += n % 10
+//     n = parseInt(n / 10)
+// }
+// console.log(ans)
+
+
+
+
+//Challenge-3
+// let n = +prompt()
+// let ans = 0
+// while (n > 0) {
+//     ans = n % 10 > ans ? n % 10 : ans
+//     n = parseInt(n / 10)
+// }
+// console.log(ans)
+
+
+
+//Challenge-4
+// let n = +prompt()
+// let ans = 0
+// while (n > 0) {
+//     ans += n % 10
+//     n = parseInt(n / 10)
+// }
+// console.log(ans)
+
+
+
+//Challenge-5
+// let n = +prompt()
+// let ans = 0
+// while (n > 0) {
+//     ans += 1
+//     n = parseInt(n / 10)
+// }
+// console.log(ans)
