a8d981a8123c3b2987ae248fae985f5fe71a65e3
BoborahimAlisherovich <boborahimrustamqulov0@gmail.com>
2026-02-24 11:03:43 +0500

    update

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-console.log(username);
-var username = "Ulugbek";
+// console.log(username);
+// var username = "Ulugbek";
 
-// console.log(age);
-// let age = 20;
+// // console.log(age);
+// // let age = 20;
 
-function greet(sayHello) {
-  sayHello();
+// function greet(sayHello) {
+//   sayHello();
+// }
+
+// greet(function () {
+//   console.log("Bu callback");
+// });
+
+// Array.prototype.getValues = function () {
+//   console.log("Bu Array prototypedan kelyapdi");
+// };
+
+// Array().getValues();
+
+
+// -----------------------masalalar-----------------------
+//Berilgan sonlarni toq va tuftlari nechta ekanligini aniqlovchi dastur tuzing. Masalan: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 sonlari uchun natija quyidagicha bo'ladi:
+
+
+
+
+console.log(2, 3, 4, 5, 6, 7, 8, 9, 10);
+var numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+var evenCount = 0;
+var oddCount = 0;
+
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    evenCount++;
+  } else {
+    oddCount++;
+  } 
 }
+console.log("Toq sonlar soni: " + oddCount);
+console.log("Tuf sonlar soni: " + evenCount);
 
-greet(function () {
-  console.log("Bu callback");
-});
 
-Array.prototype.getValues = function () {
-  console.log("Bu Array prototypedan kelyapdi");
-};
-
-Array().getValues();
