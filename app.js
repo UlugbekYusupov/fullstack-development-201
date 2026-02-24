16994589d25928a928d7b6f688634535594ad9cd
UlugbekYusupov <bilmadim.uz@gmail.com>
2026-02-24 10:51:42 +0500

    Lecture 4 practice

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-console.log(username);
-var username = "Ulugbek";
+// console.log(username);
+// var username = "Ulugbek";
 
-// console.log(age);
-// let age = 20;
+// // console.log(age);
+// // let age = 20;
 
-function greet(sayHello) {
-  sayHello();
+// function greet(sayHello) {
+//   sayHello();
+// }
+
+// greet(function () {
+//   console.log("Bu callback");
+// });
+
+// Array.prototype.getValues = function () {
+//   console.log("Bu Array prototypedan kelyapdi");
+// };
+
+// Array().getValues();
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
 }
 
-greet(function () {
-  console.log("Bu callback");
-});
+let a = 0;
+while (a < 10) {
+  console.log(a);
+  a++;
+}
 
-Array.prototype.getValues = function () {
-  console.log("Bu Array prototypedan kelyapdi");
+let b = 1;
+do {
+  console.log(b);
+  b++;
+} while (b < 10);
+
+const user = {
+  username: "Ulugbek",
+  age: 30,
+  country: "Uzbekistan",
 };
 
-Array().getValues();
+const values = Object.values(user);
+const keys = Object.keys(user);
+
+console.log(values);
+console.log(keys);
+
+for (const key in user) {
+  const element = user[key];
+  console.log(element);
+}
+
+
+
+for (const element in values) {
+  console.log(element);
+}
