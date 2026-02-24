5bb32850c07986432b318fc5ba3ac83040ab0af3
Lenovo <isayevalibek16@gmail.com>
2026-02-24 11:05:48 +0500

    first commit

---
+++ nimadir.js
@@ -0,0 +1,65 @@
+// console.log["5" + 5];
+// console.log["5" - 5];
+// console.log["5" + "2"];
+// console.log["10" / 2];
+// console.log[5 + true];
+// console.log["10" * true];
+// console.log["5" + true];
+// console.log[5 - null];
+// console.log[5 - undefined];
+
+
+
+///////////////////////// 1-mashq //////////////////////////////
+
+// let n = 10;
+
+// let juft = 0;
+// let toq = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 === 0) {
+//         juft++;
+//     } else {
+//         toq++;
+//     }
+// }
+
+
+// console.log("Juft sonlar soni:", juft);
+// console.log("Toq sonlar soni:", toq);
+
+
+
+///////////////////////// 2-mashq ///////////////////////////////
+
+// let n = Number(prompt("Son kiriting:"));
+// let reversed = 0;
+
+// while (n > 0) {
+//     let lastDigit = n % 10;
+//     reversed = reversed * 10 + lastDigit;  
+//     n = Math.floor(n / 10);
+// }
+
+// console.log("Teskari son:", reversed);
+
+
+
+
+/////////////////////////// 3-mashq //////////////////////////
+
+// let n = Number(prompt("Son kiriting:"));
+// let max = 0;
+
+// while (n > 0) {
+//     let d = n % 10;
+    
+//     if (d > max) {
+//         max = d;
+//     }
+    
+//     n = Math.floor(n / 10);
+// }
+
+// console.log(max);
