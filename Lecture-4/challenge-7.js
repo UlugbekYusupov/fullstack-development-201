88a143a78955b7cb77bfe98c4a2bd7a2a616e05a
Az1zbekx <kalilinux42331@gmail.com>
2026-02-24 11:02:33 +0500

    challenge-7

--- Lecture-4/challenge-7.js
+++ Lecture-4/challenge-7.js
@@ -3,9 +3,10 @@ for (let i = 0; i < 5; i++) {
     name = prompt()
     mark = parseInt(prompt())
     s += mark
-    if (s < 60) console.log("F")
-    else if (s < 70) console.log("D")
-    else if (s < 80) console.log("C")
-    else if (s < 90) console.log("B")
-    else console.log("A")
+    if (s < 60) console.log(name + "'s " + "mark is" + mark + " F")
+    else if (s < 70) console.log(name + "'s " + "mark is" + mark + " D")
+    else if (s < 80) console.log(name + "'s " + "mark is" + mark + " C")
+    else if (s < 90) console.log(name + "'s " + "mark is" + mark + " B")
+    else console.log(name + "'s " + "mark is " + mark + " A")
 }
+console.log("Total mark is " + s / 5)
