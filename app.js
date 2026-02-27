2c969afebc35c847d22d208f57dac81823426e4f
ogabek <uzmacbook@Ogabek.local>
2026-02-27 08:03:52 +0500

    xa

--- app.js
+++ app.js
@@ -1,117 +1,119 @@
-console.log(5 + '5')
+// console.log(5 + '5')
 
-console.log('5' - 5)
+// console.log('5' - 5)
 
-console.log('5' * 2)
-
-
-console.log('10' / 2)
-
-console.log(5 + true)
+// console.log('5' * 2)
 
 
-console.log('10' - true)
+// console.log('10' / 2)
+
+// console.log(5 + true)
 
 
-console.log('5' + true)
+// console.log('10' - true)
 
 
-console.log(5 + null)
+// console.log('5' + true)
 
 
-console.log(5 + null)
-
-console.log(5 + undefined)
+// console.log(5 + null)
 
 
-console.log(parseInt('200') + 50)
+// console.log(5 + null)
 
-let num = 25
-let str = num.toString()
-console.log(str + ' years old')
+// console.log(5 + undefined)
 
 
-console.log('false' === true)
+// console.log(parseInt('200') + 50)
 
-console.log('10' - true)
-
-
-// Execution Context nima ?
-// Executioin context bu JavaScript kodi ishlaydigan va uning uchun zarur bo'lgan barcha malumotlar (o'zgaruvchilar,funksiyalar,
-// this kalit so'zi, scope zanjir ) saqlanadigan muhit yoki konteyner. Kod har gal bajarilganda, JS dvigateli xotira va kodni boshqarish
-// uchun maxsus kontekst yaratadi.
-// Execution Context turlari: 
-// 1) Global Execution Context (GEC): kod birinchi yuklanganda yaratiladigan asosiy, yagona kontekst
-// 2) Function Execution Context (FEC): Funksiya chaqirilganda har bir chaqiruv uchun alohida yaratiladigan muhit.
-// 
-// Execution Context bosqichlari:
-//  1. Yaratish bosqichi(Phase creation): O'zgaruvchilar va funksiyalar xotiradan joy oladi (hoisting) this qiymati aniqlanadi.
-//  2. Bajarish bosqichi (Execution Phase): Kod satrma-satr o'qilib, o'zgaruvchilarga qiymatlar yuklanadi va funksiyalar ishlaydi.
-// u kodning bajarilish tartibini (Call Stack) boshqaradi.
+// let num = 25
+// let str = num.toString()
+// console.log(str + ' years old')
 
 
+// console.log('false' === true)
+
+// console.log('10' - true)
+
+
+// // Execution Context nima ?
+// // Executioin context bu JavaScript kodi ishlaydigan va uning uchun zarur bo'lgan barcha malumotlar (o'zgaruvchilar,funksiyalar,
+// // this kalit so'zi, scope zanjir ) saqlanadigan muhit yoki konteyner. Kod har gal bajarilganda, JS dvigateli xotira va kodni boshqarish
+// // uchun maxsus kontekst yaratadi.
+// // Execution Context turlari: 
+// // 1) Global Execution Context (GEC): kod birinchi yuklanganda yaratiladigan asosiy, yagona kontekst
+// // 2) Function Execution Context (FEC): Funksiya chaqirilganda har bir chaqiruv uchun alohida yaratiladigan muhit.
+// // 
+// // Execution Context bosqichlari:
+// //  1. Yaratish bosqichi(Phase creation): O'zgaruvchilar va funksiyalar xotiradan joy oladi (hoisting) this qiymati aniqlanadi.
+// //  2. Bajarish bosqichi (Execution Phase): Kod satrma-satr o'qilib, o'zgaruvchilarga qiymatlar yuklanadi va funksiyalar ishlaydi.
+// // u kodning bajarilish tartibini (Call Stack) boshqaradi.
 
 
 
-const temperatureCelsius = 25.0;
-
-const tempFahrenheit = (temperatureCelsius*(9/5)) + 32
-
-console.log("cel",temperatureCelsius)
-console.log("faren", tempFahrenheit)
 
 
-// tempFahrenheit = 27
-// console.log(tempFahrenheit)
+// const temperatureCelsius = 25.0;
 
-// const input = prompt("enter the number: ")
+// const tempFahrenheit = (temperatureCelsius*(9/5)) + 32
 
-// const cel = parseFloat(input)
-
-// const fahren = (cel * (9/5)) + 32
-
-// console.log(`${cel} is equal to ${fahren}`)
-// alert(`${cel} is equal to ${fahren}`)
+// console.log("cel",temperatureCelsius)
+// console.log("faren", tempFahrenheit)
 
 
-// let budget;
-// const input = prompt("enter number")
+// // tempFahrenheit = 27
+// // console.log(tempFahrenheit)
 
-// budget = Number(input)
+// // const input = prompt("enter the number: ")
 
+// // const cel = parseFloat(input)
 
-// const expense1 = Number(prompt("1-chi expenseni kiriting: "))
-// const expense2 = Number(prompt("2-chi expenseni kiriting: "))
-// const expense3 = Number(prompt("3-chi expenseni kiriting: "))
+// // const fahren = (cel * (9/5)) + 32
 
-// const balance = budget - (expense1 + expense2 + expense3)
-
-// if(balance < 0){
-//     alert("puling yetmaydi")
-// } else {
-//     alert("pul kop hali")
-// }
+// // console.log(`${cel} is equal to ${fahren}`)
+// // alert(`${cel} is equal to ${fahren}`)
 
 
+// // let budget;
+// // const input = prompt("enter number")
 
-console.log(!!"false" == !!"true")
-console.log(0 || "JavaScript")
-console.log(" " && 100)
-
-
-console.log(null ?? "Fallback")
-
-console.log(undefined ?? "Default Value")
+// // budget = Number(input)
 
 
-let a = "42"
-let b = "3.14"
-let c = "hello"
+// // const expense1 = Number(prompt("1-chi expenseni kiriting: "))
+// // const expense2 = Number(prompt("2-chi expenseni kiriting: "))
+// // const expense3 = Number(prompt("3-chi expenseni kiriting: "))
 
-let d = false
+// // const balance = budget - (expense1 + expense2 + expense3)
 
-let e = null
+// // if(balance < 0){
+// //     alert("puling yetmaydi")
+// // } else {
+// //     alert("pul kop hali")
+// // }
 
-let sum = a + b + d + e
 
-console.log("sum",sum)
+
+// console.log(!!"false" == !!"true")
+// console.log(0 || "JavaScript")
+// console.log(" " && 100)
+
+
+// console.log(null ?? "Fallback")
+
+// console.log(undefined ?? "Default Value")
+
+
+// let a = "42"
+// let b = "3.14"
+// let c = "hello"
+
+// let d = false
+
+// let e = null
+
+// let sum = a + b + d + e
+
+// console.log("sum",sum)
+
+console.log("sup boy")
