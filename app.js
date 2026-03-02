03c4a180d9392f5c4af02bd58c454eaf3ba92c9a
Og'abek Mullajonov <ogabek@Ogabeks-MacBook-Pro.local>
2026-03-02 09:39:01 +0500

    challenge 1

--- app.js
+++ app.js
@@ -1,119 +1,28 @@
-// console.log(5 + '5')
+// challenge 1
 
-// console.log('5' - 5)
+function greet(name){
+    console.log(`Hello ${name}`)
+}
 
-// console.log('5' * 2)
-
-
-// console.log('10' / 2)
-
-// console.log(5 + true)
+greet('ava')
 
 
-// console.log('10' - true)
+const greet2 = function(name){
+    console.log(`Hello ${name}`)
+}
 
+greet2('nigga')
 
-// console.log('5' + true)
+const greet3 = (name) => {
+    console.log(`Hello ${name}`)
+}
 
+greet3('Ngga')
 
-// console.log(5 + null)
+const obj = {
+    greet4(name){
+        console.log(`Hello ${name}`)
+    }
+}
 
-
-// console.log(5 + null)
-
-// console.log(5 + undefined)
-
-
-// console.log(parseInt('200') + 50)
-
-// let num = 25
-// let str = num.toString()
-// console.log(str + ' years old')
-
-
-// console.log('false' === true)
-
-// console.log('10' - true)
-
-
-// // Execution Context nima ?
-// // Executioin context bu JavaScript kodi ishlaydigan va uning uchun zarur bo'lgan barcha malumotlar (o'zgaruvchilar,funksiyalar,
-// // this kalit so'zi, scope zanjir ) saqlanadigan muhit yoki konteyner. Kod har gal bajarilganda, JS dvigateli xotira va kodni boshqarish
-// // uchun maxsus kontekst yaratadi.
-// // Execution Context turlari: 
-// // 1) Global Execution Context (GEC): kod birinchi yuklanganda yaratiladigan asosiy, yagona kontekst
-// // 2) Function Execution Context (FEC): Funksiya chaqirilganda har bir chaqiruv uchun alohida yaratiladigan muhit.
-// // 
-// // Execution Context bosqichlari:
-// //  1. Yaratish bosqichi(Phase creation): O'zgaruvchilar va funksiyalar xotiradan joy oladi (hoisting) this qiymati aniqlanadi.
-// //  2. Bajarish bosqichi (Execution Phase): Kod satrma-satr o'qilib, o'zgaruvchilarga qiymatlar yuklanadi va funksiyalar ishlaydi.
-// // u kodning bajarilish tartibini (Call Stack) boshqaradi.
-
-
-
-
-
-// const temperatureCelsius = 25.0;
-
-// const tempFahrenheit = (temperatureCelsius*(9/5)) + 32
-
-// console.log("cel",temperatureCelsius)
-// console.log("faren", tempFahrenheit)
-
-
-// // tempFahrenheit = 27
-// // console.log(tempFahrenheit)
-
-// // const input = prompt("enter the number: ")
-
-// // const cel = parseFloat(input)
-
-// // const fahren = (cel * (9/5)) + 32
-
-// // console.log(`${cel} is equal to ${fahren}`)
-// // alert(`${cel} is equal to ${fahren}`)
-
-
-// // let budget;
-// // const input = prompt("enter number")
-
-// // budget = Number(input)
-
-
-// // const expense1 = Number(prompt("1-chi expenseni kiriting: "))
-// // const expense2 = Number(prompt("2-chi expenseni kiriting: "))
-// // const expense3 = Number(prompt("3-chi expenseni kiriting: "))
-
-// // const balance = budget - (expense1 + expense2 + expense3)
-
-// // if(balance < 0){
-// //     alert("puling yetmaydi")
-// // } else {
-// //     alert("pul kop hali")
-// // }
-
-
-
-// console.log(!!"false" == !!"true")
-// console.log(0 || "JavaScript")
-// console.log(" " && 100)
-
-
-// console.log(null ?? "Fallback")
-
-// console.log(undefined ?? "Default Value")
-
-
-// let a = "42"
-// let b = "3.14"
-// let c = "hello"
-
-// let d = false
-
-// let e = null
-
-// let sum = a + b + d + e
-
-// console.log("sum",sum)
-
-console.log("sup boy")
+obj.greet4('xa')
