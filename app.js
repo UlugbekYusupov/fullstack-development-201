33897324e70a921449bd79ba3339012f59605ec4
Og'abek Mullajonov <ogabek@Ogabeks-MacBook-Pro.local>
2026-03-02 10:17:31 +0500

    4 5 6=

--- app.js
+++ app.js
@@ -1,15 +1,68 @@
-// challenge 3
 
-function countDigits(n){
-    let cnt = 0;
 
-    while(n > 0){
-        n = Math.floor(n/10)
-        cnt++;
+// challenge 4
+function isPalindrome(n){
+    nstr = String(n)
+    
+    const nreverse = nstr.split('').reverse().join('')
+    if(nstr === nreverse){
+        return true
     }
-
-    return cnt
+    return false
 }
 
-console.log(countDigits(1123))
 
+console.log(isPalindrome(122))
+
+
+// challenge 5
+
+function isArmstrong(n){
+    const norg = n
+    let sum = 0
+    function cntDigit(a){
+        const aS = String(a)
+        return aS.length
+    }
+    const p = cntDigit(norg)
+    while(n > 0){
+        let lastD = n%10
+        sum += lastD**p
+        n = Math.floor(n/10)
+    }
+    if(sum === norg){
+        return true
+    }
+    return false
+}
+
+console.log(isArmstrong(153))
+
+
+//   challenge 6
+
+function createCounter(initialVal){
+    let a = initialVal;
+
+    return {
+        increment: function (){
+            a += 1;
+            return a
+        },
+        decrement: function(){
+            a -= 1;
+            return a;
+        }, 
+        setNull: function(){
+            return a = 0;
+        },
+        getValue: function() {
+            return a
+        }
+    }
+}
+
+const myNumber = createCounter(9)
+myNumber.increment()
+
+console.log(myNumber.getValue())
