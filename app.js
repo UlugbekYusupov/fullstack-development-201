5f75727ad69c99a77993539697bd812aa9912da1
jumabektoshmurodov <jumabektoshmurodov1@gmail.com>
2026-03-03 11:24:32 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,106 @@
-console.log(username);
-var username = "Ulugbek";
+///1
+// function greet(name) {
+//   return "Hello, " + name + "!";
+// }
+// const greet1 = function(name) {
+//   return "Hello, " + name + "!";
+// };
+// const greet2 = (name) => {
+//   return "Hello, " + name + "!";
+// };
+// const greet3 = name => "Hello, " + name + "!";
+// const greet4 = name => `Hello, ${name}!`;
+// console.log(greet("Alice"));
+// console.log(greet1("Bob"));
+///2
+// function isPrime(n) {
+//   if (n <= 1) return false;
 
-// console.log(age);
-// let age = 20;
+//   for (let i = 2; i <= Math.sqrt(n); i++) {
+//     if (n % i === 0) return false;
+//   }
 
-function greet(sayHello) {
-  sayHello();
-}
+//   return true;
+// }
+// console.log(isPrime(2));
+///3
+// function countDigits(n) {
+//   n = Math.abs(n);
+//   let count = 0;
 
-greet(function () {
-  console.log("Bu callback");
-});
+//   do {
+//     count++;
+//     n = Math.floor(n / 10);
+//   } while (n > 0);
 
-Array.prototype.getValues = function () {
-  console.log("Bu Array prototypedan kelyapdi");
-};
+//   return count;
+// }
+// console.log(countDigits(12345));
+///4
+// function isPalindrome(n) {
+//   const str = n.toString();
+//   const reversed = str.split("").reverse().join("");
+//   return str === reversed;
+// }
+// console.log(isPalindrome(12321));
+///5
+// function isArmstrong(n) {
+//   const digits = n.toString().split("");
+//   const power = digits.length;
 
-Array().getValues();
+//   const sum = digits.reduce((acc, digit) => {
+//     return acc + Math.pow(Number(digit), power);
+//   }, 0);
+
+//   return sum === n;
+// }
+// console.log(isArmstrong(153));
+///6
+// function createCounter(start = 0) {
+//   let count = start;
+
+//   return {
+//     increment() {
+//       count++;
+//     },
+//     decrement() {
+//       count--;
+//     },
+//     getCount() {
+//       return count;
+//     }
+//   };
+// }
+///7
+// function once(fn) {
+//   let called = false;
+//   let result;
+
+//   return function(...args) {
+//     if (!called) {
+//       called = true;
+//       result = fn(...args);
+//     }
+//     return result;
+//   };
+// }
+// function salom() {
+//   console.log("Ishladi!");
+//   return 42;
+// }
+
+// const birMarta = once(salom);
+// console.log(birMarta()); 
+// console.log(birMarta()); 
+// console.log(birMarta());
+///8
+// function lazyAdder(a) {
+//     return function(b) {
+//         return a + b;
+//     };
+// }
+
+// const add5 = lazyAdder(5);
+
+// console.log(add5(10)); 
+// console.log(add5(20)); 
