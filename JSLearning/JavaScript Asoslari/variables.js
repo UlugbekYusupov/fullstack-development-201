8c7dc806d7b27b8cad5e8e8dce07218a907e5273
Abbosjon <Abbosjondev@gmail.com>
2026-03-06 10:36:32 +0500

    06.0

---
+++ JSLearning/JavaScript Asoslari/variables.js
@@ -0,0 +1,11 @@
+// 1-Mashq:
+// Siz o'zingiz haqingizda ma'lumot beruvchi dastur yozyapsiz.
+// Ismingizni const yordamida e'lon qiling va unga o'z ismingizni qiymat qilib bering.
+// Yoshingizni let yordamida e'lon qiling.
+// Yoshingizni bittaga oshirib (masalan, 20 dan 21 ga) o'zgartiring.
+// Natijalarni console.log() yordamida ekranga chiqaring.
+
+const name = "Azizbek";
+let age = 18;
+age++;
+console.log(`Mening ismim ${name}, yoshim ${age}`)
