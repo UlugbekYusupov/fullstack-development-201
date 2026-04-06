0c7acf3aecdb8979de0b50e7c2c88d60d452f806
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2026-04-06 10:19:48 +0500

    auth login endpoints

--- tech_house_backend/src/controllers/authController.js
+++ tech_house_backend/src/controllers/authController.js
@@ -1,29 +1,52 @@
-import users from "../data/db.js";
-import generateIde from "../utils/generateId.js";
-import bcrypt from "bcrypt";
+    import users from "../data/db.js";
+    import generateIde from "../utils/generateId.js";
+    import bcrypt from "bcrypt";
+    import jwt from "jsonwebtoken";
 
-export function register(req, res){
-    console.log(`Checking request body ${req.body}`);
 
-    const {username,  email, password } = req.body;
+    export async function register(req, res){
 
-    if (!username || !email || !password) {
-        return res.status(400).json({
-            message: "Username, eamil and password required",
-        });
+        const {username,  email, password } = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "Username, eamil and password required",
+            });
+        }
+
+        const existingUser = users.find((user) => user.email === email);
+        if (existingUser) {
+            return res.status(409).json({message: "User already exist"})
+        }
+        const passwordHash = await bcrypt.hash(password, 10)
+        const newUser = {
+            id: generateIde(users),
+            username,
+            email,
+            password: passwordHash,
+        };
+        users.push(newUser)
+        return res.status(201).json({message: "User created"});
+        //console.log(`User info ${username} ${email} ${password}`);
     }
 
-    const passwordHash = bcrypt.hash(password, 10)
-    const user = {
-        id: generateIde(users),
-        username,
-        email,
-        password
+    export async function login(req, res) {
+        const {password, email} = req.body
 
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Emailil and password required",
+            });
+        }
+
+        const existingUser = users.find((user) => user.email === email);
+        console.log(existingUser.password)
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
+        if (!existingUser || !isPasswordCorrect) {
+            return res.status(400).json({message: "Email or password is not correct, Please check!"})
+        }
+
+        const  token = jwt.sign({email}, "JWT Secret key", {expiresIn: "1h"})
+
+        return res.status(200).json({message: "Login successfully", token: token})
     }
-    console.log(`User info ${username} ${email} ${password}`);
-}
-
-export function login() {
-
-}
