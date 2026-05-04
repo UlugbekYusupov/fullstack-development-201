9ba7b5538f4ea4ed5c7280dd8efe63efccf12055
Abbosjon <Abbosjondev@gmail.com>
2026-05-04 06:56:09 +0500

    Express JS | Sessions

--- Express JS/src/index.js
+++ Express JS/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 import userRoutes from "./routes/userRoutes.js";
 import cookieRoutes from "./routes/cookieRoutes.js"
 import { loggingMiddleware } from "./middlewares/loggingMiddleware.js";
@@ -9,6 +10,16 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(session({
+  secret: 'hello-world-from-dev',   // Session ID ni shifrlash uchun maxfiy kalit.
+  resave: false,                    // O'zgartirish bo'lmasa qayta saqlashni o'chirish
+  saveUninitialized: false,         // Bo'sh sessionni saqlamasin
+  cookie: {
+    maxAge: 1000 * 60 * 60,    // 1 soat
+    httpOnly: true,            // JS orqali o'qib bo'lmasin
+    secure: false              // HTTPS bo'lsa true qilish mumkin
+  }
+}));
 app.use(loggingMiddleware);
 
 app.use("/users", userRoutes);
